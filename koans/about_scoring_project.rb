b63fea1e540b83093c36e224c4f063353700117b
Aaron Glasenapp <glassbead@gmail.com>
2014-08-15 20:19:01 -0600

    completed all stages of about_scoring_project
    * completely rewrote the scoring method
    * created a greed class
    * broke down the scoring method into smaller bites
    * wrote a score_ones method
    * wrote a score_fives method
    * wrote a score_others method
    * wrote a score method that looped through all numbers and scored them
    * appropriately
    
    [#75411440]

--- koans/about_scoring_project.rb
+++ koans/about_scoring_project.rb
@@ -29,52 +29,110 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #
 # Your goal is to write the score method.
 
-def score(dice)
-  return 0 if dice.length == 0
-  a, b, c, d, e = dice.sort # put dice in ascending order
-  return 50 if dice.include?(5)
+class Greed
+  def initialize(dice)
+    @dice = dice.sort
+    @score = 0
+  end
+
+  def score
+    num = 1
+    while @dice.length > 0
+      if num == 1
+        score_ones
+
+      elsif num == 5
+        score_fives
+
+      else # if num == 2, 3, 4, or 6
+        score_others(num)
+      end
+      num += 1
+    end
+    @score
+  end
+
+  private
+
+  def score_ones
+    count = @dice.select { |die| die == 1 }
+    if count.length == 5
+      @score += 1200
+    elsif count.length == 4
+      @score += 1100
+    elsif count.length == 3
+      @score += 1000
+    else
+      @score += 100 * count.length
+    end
+    @dice.delete(1)
+  end
+
+  def score_fives
+    count = @dice.select { |die| die == 5 }
+    if count.length == 5
+      @score += 600
+    elsif count.length == 4
+      @score += 550
+    elsif count.length == 3
+      @score += 500
+    else
+      @score += 50 * count.length
+    end
+    @dice.delete(5)
+  end
+
+  def score_others(num)
+    count = @dice.select { |die| die == num }
+    if count.length >= 3
+      @score += num * 100
+    end
+    @dice.delete(num)
+  end
 
 end
 
+
+
 class AboutScoringProject < Neo::Koan
   def test_score_of_an_empty_list_is_zero
-    assert_equal 0, score([])
+    assert_equal 0, Greed.new([]).score
   end
 
   def test_score_of_a_single_roll_of_5_is_50
-    assert_equal 50, score([5])
+    assert_equal 50, Greed.new([5]).score
   end
 
   def test_score_of_a_single_roll_of_1_is_100
-    assert_equal 100, score([1])
+    assert_equal 100, Greed.new([1]).score
   end
 
   def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
-    assert_equal 300, score([1,5,5,1])
+    assert_equal 300, Greed.new([1,5,5,1]).score
   end
 
   def test_score_of_single_2s_3s_4s_and_6s_are_zero
-    assert_equal 0, score([2,3,4,6])
+    assert_equal 0, Greed.new([2,3,4,6]).score
   end
 
   def test_score_of_a_triple_1_is_1000
-    assert_equal 1000, score([1,1,1])
+    assert_equal 1000, Greed.new([1,1,1]).score
   end
 
   def test_score_of_other_triples_is_100x
-    assert_equal 200, score([2,2,2])
-    assert_equal 300, score([3,3,3])
-    assert_equal 400, score([4,4,4])
-    assert_equal 500, score([5,5,5])
-    assert_equal 600, score([6,6,6])
+    assert_equal 200, Greed.new([2,2,2]).score
+    assert_equal 300, Greed.new([3,3,3]).score
+    assert_equal 400, Greed.new([4,4,4]).score
+    assert_equal 500, Greed.new([5,5,5]).score
+    assert_equal 600, Greed.new([6,6,6]).score
   end
 
   def test_score_of_mixed_is_sum
-    assert_equal 250, score([2,5,2,2,3])
-    assert_equal 550, score([5,5,5,5])
-    assert_equal 1100, score([1,1,1,1])
-    assert_equal 1200, score([1,1,1,1,1])
-    assert_equal 1150, score([1,1,1,5,1])
+    assert_equal 250, Greed.new([2,5,2,2,3]).score
+    assert_equal 550, Greed.new([5,5,5,5]).score
+    assert_equal 1100, Greed.new([1,1,1,1]).score
+    assert_equal 1200, Greed.new([1,1,1,1,1]).score
+    assert_equal 1150, Greed.new([1,1,1,5,1]).score
   end
 
 end
