78dea15b7a43ccf530b7729283edbc366eaa4813
Aaron Glasenapp <glassbead@gmail.com>
2014-08-18 14:09:11 -0600

    completed all stages (5/5) of about_classes
    * wrote the DiceSet class
    * included an initialize method
    * included a roll method
    * included a value method
    
    [#75411446]

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -1,10 +1,22 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-# Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+
+class DiceSet
+  def initialize
+    @rolls = []
+  end
+
+  def roll(number_of_dice)
+    @rolls = []
+    number_of_dice.times do
+      @rolls << 1 + rand(6)
+    end
+  end
+
+  def values
+    @rolls
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -48,6 +60,16 @@ class AboutDiceProject < Neo::Koan
     # If the rolls are random, then it is possible (although not
     # likely) that two consecutive rolls are equal.  What would be a
     # better way to test this?
+    # you could test the objet_id of dice.values before and after:
+    #
+    # dice = DiceSet.new
+    #
+    # dice.roll(5)
+    # first_id = dice.values.object_id
+    #
+    # dice.roll(5)
+    # second_id = dice.values.object_id
+    # assert_not_equal first_id, second_id
   end
 
   def test_you_can_roll_different_numbers_of_dice
